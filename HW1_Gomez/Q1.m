8b389719374b1585eab0e9d521fe40437fa8ebe5
Miguel <u1318856@eng.utah.edu>
2023-08-26 22:15:02 -0600

    updated example and the second to last part of problem 3

--- HW1_Gomez/Q1.m
+++ HW1_Gomez/Q1.m
@@ -14,20 +14,61 @@ z = convert(data(:,3));
 
 plot_data(x,y,z,'After conversion');
 
-%% Next is to set up the sampling
-% need a cosine signal cos(2pift) 
-t = linspace(-.5,.5,length(x));
-c = (@(f,t) cos(2*pi*f*t));
+%%
+
 figure
-% this plot shows the cos using the first 100 frequencies
-%figure(Visible="on",Name='custom cos')
-for i = 1:1e2
-    plot(t,c(i,t));
-    plot_title = sprintf('freq = %.2f',i);
-    title(plot_title)
-    pause(.1)
+%% Next is to set up the sampling
+% need a cosine signal cos(2*pi*f*t) 
+t = linspace(-0.5,0.5,length(x));
+c = (@(f,t) cos(2*pi*f*t));
+
+% from the MANUAL.txt file included in the data. 
+Fs = 32;
+
+
+t_span = length(x)*(1/Fs)
+
+freq = [5 30 60 80];
+periods = freq.^(-1);
+
+n_s = floor(t_span./periods);
+
+for i = 1:length(freq)
+    t_s = 0:t_span/n_s(i):(t_span - 1);
+    plot(0:t_span/Fs:t_span,cos(0:t_span/Fs:t_span));
+    hold on;
+    stem(t_s, cos(t_s));
+    hold off;
+    pause(.01)
+    
 end
 
+
+% this plot shows the cos using the first 100 frequencies. uncomment code
+% below to see the plot
+%figure(Visible="on",Name='custom cos')
+% for i = 1:1e2
+%     plot(t,c(i,t));
+%     plot_title = sprintf('freq = %.2f',i);
+%     title(plot_title)
+%     pause(.1)
+% end
+%%
+figure()
+for i = [30]
+    samples = find(c(i,linspace(0,length(x)*(1/32),length(x))) >= .98);
+    vec = zeros(length(x),1);
+    vec(samples) = 1;
+    plot(1:length(vec),vec.*x,'*')
+    hold on 
+    plot(1:length(x),x)
+    plot(1:length(x),c(i,linspace(-.5,.5,length(x))))
+    hold off
+    plot_title = sprintf('sampling freq = %.2f',i);
+    title(plot_title)
+    ylim([-15 15])
+    pause(.2)
+end
 %%
 % according to the Readme in the repo, there is a defined max and min
 % From the file:
