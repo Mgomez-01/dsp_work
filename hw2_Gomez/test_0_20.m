ad823842e9b0a7058439719ab3c1e3e7d2325fda
Miguel Gomez <u1318856@eng.utah.edu>
2023-09-18 09:44:47 -0600

    renamed folders to use lowercase letters for easier traversal

---
+++ hw2_Gomez/test_0_20.m
@@ -0,0 +1,23 @@
+%% Automatically created temporary file created to run-region
+
+x = data(:,1);
+y = data(:,2);
+z = data(:,3);
+figure
+lti_test2(x,1)
+figure
+lti_test2(y,1)
+figure
+lti_test2(z,1)
+
+x = converted_data(:,1);
+y = converted_data(:,2);
+z = converted_data(:,3);
+
+figure
+lti_test2(x,1)
+figure
+lti_test2(y,1)
+figure
+lti_test2(z,1)
+%%
