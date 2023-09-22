0bb6a0bf26f8493d805368bcb2a546d5fb868c52
Miguel Gomez <u1318856@eng.utah.edu>
2023-09-21 22:12:04 -0600

    updating files for hw3

--- hw3_Gomez/tex/auto/main.el
+++ hw3_Gomez/tex/auto/main.el
@@ -5,6 +5,16 @@
                      '(("exam" "a4paper" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("fontenc" "T1") ("xcolor" "dvipsnames") ("algorithm2e" "linesnumbered" "ruled" "vlined")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (TeX-run-style-hooks
     "latex2e"
     "exam"
@@ -44,6 +54,10 @@
     '("laplace" 1)
     '("cc" 2)
     '("invlaplace" 1)
-    "myvspace"))
+    "myvspace")
+   (LaTeX-add-environments
+    '("eqnsection" 2))
+   (LaTeX-add-enumitem-newlists
+    '("myenumerate" "enumerate")))
  :latex)
 
