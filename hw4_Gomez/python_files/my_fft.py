184956d092f79417dc1eb34f25bfd861f5e30245
Miguel <u1318856@eng.utah.edu>
2023-10-15 18:02:36 -0600

    adding in an attempt at doing this with python

--- hw4_Gomez/python_files/my_fft.py
+++ hw4_Gomez/python_files/my_fft.py
@@ -1,8 +1,11 @@
 import numpy as np
+from rich import print
+
 
 def dfs(x, N):
     X = np.fft.fft(x)  # Use NumPy's FFT function to compute DFS coefficients
     return X
+
 
 def idfs(X, N):
     x = np.fft.ifft(X)  # Use NumPy's IFFT function to compute IDFS
@@ -16,7 +19,7 @@ N = 9
 X = dfs(x, N)
 
 # Print the DFS coefficients
-print(f"X:{X}")
+print(f"X: {X}")
 
 
 # Example usage idfs
@@ -24,5 +27,5 @@ N = len(X)
 
 x_reconstructed = idfs(X, N)
 # Print the reconstructed signal
-print(f"x_reconstructed:{x_reconstructed}")
+print(f"x_reconstructed: {x_reconstructed}")
 
